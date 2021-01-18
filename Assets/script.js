ecdcb945d490df4074aa4f7f8970ce4a4402d073
kofithebass <kofi@kofihayford.com>
2021-01-18 18:34:52 -0500

    worked on major fixes of javascript
    Javaascript updates. Moved parts of the HTML to the JS file. added various functions. Added event listener to all buttons. Used console log and alerts to ensure they were working. Was ablet to get the questions to start iterating. Fixed a bug where the alert was popping up multiple times. Faced issues with answers registering and game over function working. Was able to get the score to start increasing with correct answers.

--- Assets/script.js
+++ Assets/script.js
@@ -1,41 +1,23 @@
-// //--------Declare Key Variables for Questions, Score, & Timer-----
-// let questionPrompt = "";
-// let previousScores;
-// let questionMarker = 0;
-// let scoreHistory = [];
+//--------Declare Key constiables for Questions, Score, & Timer-----
+let score = 0
+const timeInterval = 10
+let lastQuestionMarker
+let correctAnswer
+let currentQuestionMarker = 0
 
+//-------Connect the DOM -------
+const startQuizBtn = document.getElementById("startquizbtn")
+const response = document.querySelector("response")
+const timerElement = document.getElementById("timer")
+const gameOver = () => {
+    alert("Game Over")
+}
 
-// let currentQuestionMarker = 0
-// let timer = 60
-// let score = 0
-// let timeInterval
-// let lastQuestionMarker = quizQuestions.length
-// let correct
-
-
-
-// //-------Connect the DOM -------
-// var startQuiz = document.getElementById("#startquizbtn")
-// var questionText = document.getElementById("question")
-// var option1 = document.getElementById("optionA")
-// var option2 = document.getElementById("optionB")
-// var option3 = document.getElementById("optionC")
-// var option4 = document.getElementById("optionD")
-// var option5 = document.getElementById("optionE")
-// var response = document.querySelector("response")
-// var timer = document.getElementById("timer")
-
-
-
-// //------Create questions and options for user to select from------// 
-// let quizQuestions = [{
+//------Create questions and options for user to select from------// 
+// const quizQuestions = [{
 //     question: "What element typically contains the link to css styling?",
-//     optionA: "<head>",
-//     optionB: "<footer>",
-//     optionC: "<title>",
-//     optionD: "<body>",
-//     optionE: "<all the above>",
-//     correctAnswer: "optionA"
+//     options: ["<head>", "<footer>", "<title>", "<body>", "<all the above>",],
+//     correctAnswer: "<head>"
 // },
 // {
 //     question: "To activate a function what type of brackets should be used?",
@@ -55,12 +37,11 @@
 //     optionE: "Noe of the above",
 //     correctAnswer: "optionA"
 // },
-
 // {
 //     question: "What is a correct initial part of a for loop?",
 //     optionA: "for {i = 0}; {i < cars.length}[i++] )",
 //     optionB: "for (i = 0; i < cars.length; i++)",
-//     optionC: "for (i > "number" ; i >= cars.length; ++i)",
+//     optionC: "for (i > 'val' ; i >= cars.length; ++i)",
 //     optionD: "for (i += 0; i  cars_length; i__)",
 //     optionE: "None of the above",
 //     correctAnswer: "optionB"
@@ -70,13 +51,132 @@
 //     optionA: "alertBox('{Hello World}')",
 //     optionB: "msgBox('Hello World')",
 //     optionC: "Option A and Option B",
-//     optionD: "alert("Hello World")",
+//     optionD: "alert('Hello, World')",
 //     optionE: "None of the above",
 //     correctAnswer: "optionD"
 // },
+// ]
 
-// }]
+
+const startQuiz = () => {
+    let timer = 60
+    //change options from key value pairs into arrays for 2nd questions to the end //
+    const quizQuestions = [{
+        question: "What element typically contains the link to css styling?",
+        options: ["<head>", "<footer>", "<title>", "<body>", "<all the above>",],
+        correctAnswer: "<head>"
+    },
+    {
+        question: "To activate a function what type of brackets should be used?",
+        options: ["<Curly Brackets{}>", "<Square Brackets>", "<Parentheses ()>", "<None of the above>", "<All the above>",],
+        correctAnswer: "<Parentheses ()>"
+    },
+    {
+        question: "What are the options with a confirm method?",
+        options: ["Ok & Cancel", "Yes & No", "Maybe, Sometimes, Always", "OptionA and Option B", "Noe of the above",],
+        correctAnswer: "Ok & Cancel"
+    },
+    {
+        question: "What is a correct initial example of a for loop?",
+        options: ["for {i = 0}; {i < cars.length}[i++] )", "for (i = 0; i < cars.length; i++)", "for (i > 'val' ; i >= cars.length; ++i)", "for (i += 0; i  cars_length; i__)", "None of the above",],
+        correctAnswer: "for (i = 0; i < cars.length; i++)"
+    },
+    {
+        question: "How do you write 'Hello World' in an alert box?",
+        options: ["alertBox('{Hello World}')", "msgBox('Hello World')", "Option A and Option B", "alert('Hello, World')", "None of the above",],
+        correctAnswer: "alert('Hello, World')"
+    },
+    ]
+    //when the button is clicked, the timer should start, and a new randomly selected question should be displayed
+    //The timer should count down by 1 second at a time starting from 60 seconds and should stop at zero(0). When the timer hits zero, the quiz is over. 
 
 
-// //---- Configure the Start Quiz Button to Begin the Game ------
-// startButton.addEventListener("click", startGame);
+
+    // Create a function that renders/presents the questions and renders the answers //
+    const renderQuestion = (quizQuestions, currentQuestionMarker) => {
+        const questionText = document.getElementById("question")
+        questionText.textContent = quizQuestions[currentQuestionMarker].question
+        for (let i = 0; i < quizQuestions[currentQuestionMarker].options.length; i++) {
+            let option = document.getElementById("option" + (i + 1))
+            option.textContent = quizQuestions[currentQuestionMarker].options[i]
+        }
+
+    }
+
+    const timerInterval = setInterval(() => {
+        timer--
+        timerElement.textContent = timer;
+        //Todo: Add conditional statements for what it does when it reaches zero for Game Over. This will need it's own function. 
+        if (timer <= 0) {
+            clearInterval(timerInterval)
+            //todo: need to add gameover function here 
+        }
+    }, 1000)
+
+    let questionContent = `<h2 class="question" id="questionHdr">Question</h2>
+    <br>
+
+    <div class="questionCntnt" id="question"> Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+        Reiciendis, quae.
+        Perspiciatis facere dolore blanditiis debitis natus? </div>
+    <br>
+
+    <div class="ui vertical buttons">
+        <button class="options ui button answer " id="option1"> Option A - Lorem, ipsum dolor
+            sit amet
+            consectetur adipisicing eli</button>
+        <br>
+        <button class="ui button answer options" id="option2"> Option B - Lorem, ipsum dolor
+            sit amet
+            consectetur adipisicing eli</button>
+        <br>
+        <button class="ui button answer options" id="option3"> Option C - Lorem, ipsum dolor
+            sit amet
+            consectetur adipisicing eli</button>
+        <br>
+        <button class="ui button answer options" id="option4"> Option D - Lorem, ipsum dolor
+            sit amet
+            consectetur adipisicing eli</button>
+        <br>
+        <button class="ui button answer options" id="option5"> Option E - Lorem, ipsum dolor
+            sit amet
+            consectetur adipisicing eli</button>
+        <br>
+    </div>`
+
+    document.querySelector(".mainContent").innerHTML = questionContent
+    document.querySelectorAll("button").forEach((answerBtn) => {
+        answerBtn.addEventListener("click", (event) => {
+            if (quizQuestions.length - 1 > currentQuestionMarker) {
+                if (event.target.innerText === quizQuestions[currentQuestionMarker].correctAnswer) {
+                    alert("Correct")
+                    score += 10
+                    document.querySelector("#score").innerText = score
+                }
+
+                else {
+                    alert("Wrong")
+                    timer -= 10
+                    document.querySelector("#timer").innerText = timer
+                }
+                currentQuestionMarker++
+                renderQuestion(quizQuestions, currentQuestionMarker)
+
+            }
+            else { gameOver() }
+        })
+    })
+    countdown(timer)
+    renderQuestion(quizQuestions, currentQuestionMarker)
+    startQuizBtn.style.display = "none"
+
+}
+// the Current question marker increments by one whenever a user answers a question
+// when the current question marker becomes = or more than the question QuestionQuiz.length then it should be gameover. 
+// Check answer function that gives them 1 point if they are right. If they are wrong, the timer loses 10s per the Interval and the current queston marker should go up by 1
+// Then call the Render question again. 
+//when the game is over. Display the score and have an iput to submit your initials which stores your reponse in local storage. After that happens it should display the score history which is also 
+// A button on the high score page should then reset the quiz back to the beginning. 
+
+//---- Configure the Start Quiz Button to Begin the Game ------
+startQuizBtn.addEventListener("click", startQuiz);
