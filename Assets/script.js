ccdce4151603f1cd9108b335556e24813de32ef5
kofithebass <kofi@kofihayford.com>
2021-01-18 15:40:07 -0500

    updated Js and index files
    updated Js and index files

--- Assets/script.js
+++ Assets/script.js
@@ -1,23 +1,82 @@
-//--------Declare Key Variables-----
-let questionPrompt = "";
-let score = 0
-let previousScores;
-let timer = 60
-let questionIndex = 0;
-let scoreHistory = [];
+// //--------Declare Key Variables for Questions, Score, & Timer-----
+// let questionPrompt = "";
+// let previousScores;
+// let questionMarker = 0;
+// let scoreHistory = [];
 
 
-//-------Connect the DOM -------
-var startQuiz = document.getElementById("#startquizbtn")
-var questionText = document.getElementById("question")
-var option1 = document.getElementById("optionA")
-var option2 = document.getElementById("optionB")
-var option3 = document.getElementById("optionC")
-var option4 = document.getElementById("optionD")
-var option5 = document.getElementById("optionE")
-var response = document.querySelector("response")
-var timer = document.getElementById("timer")
+// let currentQuestionMarker = 0
+// let timer = 60
+// let score = 0
+// let timeInterval
+// let lastQuestionMarker = quizQuestions.length
+// let correct
 
 
-//---- Configure the Start Quiz Button to Begin the Game ------
-startButton.addEventListener("click", startGame);
+
+// //-------Connect the DOM -------
+// var startQuiz = document.getElementById("#startquizbtn")
+// var questionText = document.getElementById("question")
+// var option1 = document.getElementById("optionA")
+// var option2 = document.getElementById("optionB")
+// var option3 = document.getElementById("optionC")
+// var option4 = document.getElementById("optionD")
+// var option5 = document.getElementById("optionE")
+// var response = document.querySelector("response")
+// var timer = document.getElementById("timer")
+
+
+
+// //------Create questions and options for user to select from------// 
+// let quizQuestions = [{
+//     question: "What element typically contains the link to css styling?",
+//     optionA: "<head>",
+//     optionB: "<footer>",
+//     optionC: "<title>",
+//     optionD: "<body>",
+//     optionE: "<all the above>",
+//     correctAnswer: "optionA"
+// },
+// {
+//     question: "To activate a function what type of brackets should be used?",
+//     optionA: "<Curly Brackets{}>",
+//     optionB: "<Square Brackets>",
+//     optionC: "<Parentheses ()>",
+//     optionD: "<None of the above>",
+//     optionE: "<All the options>",
+//     correctAnswer: "optionC"
+// },
+// {
+//     question: "What are the options with a confirm method?",
+//     optionA: "Ok & Cancel",
+//     optionB: "Yes & No",
+//     optionC: "Maybe, Sometimes, Always",
+//     optionD: "OptionA and Option B",
+//     optionE: "Noe of the above",
+//     correctAnswer: "optionA"
+// },
+
+// {
+//     question: "What is a correct initial part of a for loop?",
+//     optionA: "for {i = 0}; {i < cars.length}[i++] )",
+//     optionB: "for (i = 0; i < cars.length; i++)",
+//     optionC: "for (i > "number" ; i >= cars.length; ++i)",
+//     optionD: "for (i += 0; i  cars_length; i__)",
+//     optionE: "None of the above",
+//     correctAnswer: "optionB"
+// },
+// {
+//     question: "How do you write 'Hello World' in an alert box?",
+//     optionA: "alertBox('{Hello World}')",
+//     optionB: "msgBox('Hello World')",
+//     optionC: "Option A and Option B",
+//     optionD: "alert("Hello World")",
+//     optionE: "None of the above",
+//     correctAnswer: "optionD"
+// },
+
+// }]
+
+
+// //---- Configure the Start Quiz Button to Begin the Game ------
+// startButton.addEventListener("click", startGame);
